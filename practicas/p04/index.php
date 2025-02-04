4d9d3ac0767e21b0e9976a04bc186254516e439e
ValePes <valeriapes2205@gmail.com>
2025-02-04 16:17:02 -0600

    Ejercicio 2 completo, asignar y mostrar variables

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -21,18 +21,59 @@
     echo '<h3> Explicación:</h3>';
     echo '<ul>';
         echo '<li><em>$_myvar:</em> comienza con $ seguido de un guion bajo</i>';
-        echo '<li><en>$_7var:</em> comienza con $ seguido de un guion bajo, lo cual es correcto </i>';
+        echo '<li><em>$_7var:</em> comienza con $ seguido de un guion bajo, lo cual es correcto </i>';
         echo '<li><em>myvar:</em>  todas las variables deben comenzar con $</i>';
-        echo '<li><em>$myvar</em> comienza con $ y sigue con un letra</i>';
-        echo '<li><em>$var7</em> comienza con $ y sigue con un letra</i>';
-        echo '<li><em>$_element1</em> comienza con $ seguido de un guion bajo, lo cual es correcto</i>';
-        echo '<li><em>$house*5</em> los nombres de variables solo pueden contener letras, números y guion bajo, pero no otros símbolos</i>';
+        echo '<li><em>$myvar:</em> comienza con $ y sigue con un letra</i>';
+        echo '<li><em>$var7:</em> comienza con $ y sigue con un letra</i>';
+        echo '<li><em>$_element1:</em> comienza con $ seguido de un guion bajo, lo cual es correcto</i>';
+        echo '<li><em>$house*5:</em> los nombres de variables solo pueden contener letras, números y guion bajo, pero no otros símbolos</i>';
     echo '</ul>';
     ?>
 
     <h2>Ejercicio 2</h2>
     <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
-    
+    <ul>
+        <li>$a = “ManejadorSQL”</li>
+        <li>$b = 'MySQL’</li>
+        <li>$c = &$a</li>
+    </ul>
+    <?php
+        $a = "ManejadorSQL";
+        $b = 'MySQL';  
+        $c = &$a; 
+        //Mostramos el contenido de cada variable
+        echo '<p>Contendio de cada variable: </p>';
+        echo '<ul>';
+            echo "$a";
+            echo '<br>';
+            echo "$b";
+            echo '<br>';
+            echo "$c"; 
+        echo '</ul>';
+
+        echo '<p>Agrega al código actual las siguientes asignaciones: </p>';
+        echo '<ul>';
+            echo '<li>$a = “PHP server”</li>';
+            echo '<li>$b = &$a</li>';
+        echo '</ul>';
+        //Agregamos las asignaciones
+        $a = "PHP server";
+        $b = &$a;
+        //mostramos el contenido
+        echo '<p> Vuelve a mostrar el contenido </p>';
+        echo '<ul>';
+            echo "$a";
+            echo '<br>';
+            echo "$b";
+        echo '</ul>';
+
+        echo '<p>Describe y muestra en la página obtenida qué ocurrió en el segundo bloque de asignaciones:</p>';
+        echo '<ol>';
+            echo '<li>Se declara la variable "$a= PHP server"</li>';
+            echo '<li>Se declara la variable "$b= &$a", pero con una asignación por referencia (&$a) lo que significa que tambien alamcena "PHP server"</li>';
+        echo '</ol>'
+    ?>
+
     
 </body>
 </html>
