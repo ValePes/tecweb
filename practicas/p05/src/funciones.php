821a2febc547ad94cc23dc7d0641679738f6c67a
ValePes <valeriapes2205@gmail.com>
2025-02-05 14:49:07 -0600

    Ejercicio 2 completo, funcion generarSecuencia

--- practicas/p05/src/funciones.php
+++ practicas/p05/src/funciones.php
@@ -10,4 +10,22 @@
                 echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
             }
     }
+
+    function generarSecuencia(){
+        $matriz = [];
+        $iteraciones = 0;
+        $totalNumeros = 0;
+
+        do {
+            $iteraciones++;
+            $fila = [rand(1, 999), rand(1, 999), rand(1, 999)];
+            $totalNumeros += 3;
+            $matriz[] = $fila;
+        } while (!($fila[0] % 2 != 0 && $fila[1] % 2 == 0 && $fila[2] % 2 != 0));
+        
+        // Mostrar la matriz
+        foreach ($matriz as $fila) {
+            echo implode(", ", $fila) . "<br>";
+        }
+    }
 ?>
