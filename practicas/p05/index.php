f6c9f1c2898a92c0876b39ce74ca0e25f4a65ac7
ValePes <valeriapes2205@gmail.com>
2025-02-06 15:34:36 -0600

    Ejercicio 4 completo, arreglo y tabla que muestra un indice

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -41,7 +41,13 @@
         }
     ?>
 
-    
+    <h2>Ejercicio 4</h2>
+    <p>4. Crear un arreglo cuyos índices van de 97 a 122 y cuyos valores son las letras de la ‘a’ a la ‘z’. Usa la función chr(n) que devuelve el caracter cuyo código ASCII es n para poner el valor en cada índice.</p>
+    <?php
+        require_once __DIR__ .'/src/funciones.php';
+        $arreglo=arreglo_Indice();
+        tabla($arreglo);
+    ?>
 
     <h2>Ejemplo de POST</h2>
     <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
@@ -58,6 +64,8 @@
             echo $_POST["email"];
         }
     ?>
+
+    
 
 </body>
 </html>
