bf2f6a5d13caaed40c45df07d90635558502239d
ValePes <valeriapes2205@gmail.com>
2025-02-13 12:57:33 -0600

    El script ya lo muestra en tabla pero no muestra los demas objetos menor al tope

--- practicas/p07-base/get_productos_xhtml.php
+++ practicas/p07-base/get_productos_xhtml.php
@@ -1,97 +1,83 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
 "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
-<?php
-    header("Content-Type: application/json; charset=utf-8"); 
-    $data = array();
 
-	if(isset($_GET['tope']))
-    {
+    <?php
+  
+        if(isset($_GET['tope']))
+         {
 		$tope = $_GET['tope'];
-    }
-    else
-    {
+        }
+        else
+        {
         die('Parámetro "tope" no detectado...');
-    }
+        }
 
-	if (!empty($tope))
-	{
-		/** SE CREA EL OBJETO DE CONEXION */
-		@$link = new mysqli('localhost', 'root', 'Lepuchis22', 'marketzone');
-        /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
-
-		/** comprobar la conexión */
-		if ($link->connect_errno) 
-		{
-			die('Falló la conexión: '.$link->connect_error.'<br/>');
-			//exit();
-		}
-
-		/** Crear una tabla que no devuelve un conjunto de resultados */
-		if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") ) 
-		{
-            /** Se extraen las tuplas obtenidas de la consulta */
-			$row = $result->fetch_all(MYSQLI_ASSOC);
-
-            /** Se crea un arreglo con la estructura deseada */
-            foreach($row as $num => $registro) {            // Se recorren tuplas
-                foreach($registro as $key => $value) {      // Se recorren campos
-                    $data[$num][$key] = utf8_encode($value);
-                }
+        if (!empty($tope))
+        {
+            /** SE CREA EL OBJETO DE CONEXION */
+            @$link = new mysqli('localhost', 'root', 'Lepuchis22', 'marketzone');	
+    
+            /** comprobar la conexión */
+            if ($link->connect_errno) 
+            {
+                die('Falló la conexión: '.$link->connect_error.'<br/>');
+                    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
             }
-
-			/** útil para liberar memoria asociada a un resultado con demasiada información */
-			$result->free();
-		}
-
-		$link->close();
-    }
+    
+            /** Crear una tabla que no devuelve un conjunto de resultados */
+            if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") ) 
+            {
+                $row = $result->fetch_array(MYSQLI_ASSOC);
+                /** útil para liberar memoria asociada a un resultado con demasiada información */
+                $result->free();
+            }
+    
+            $link->close();
+        }
     ?>
-	<head>
+    <head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Producto</title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	</head>
-	<body>
-		<h3>PRODUCTO</h3>
+    <body>
+    <h3>PRODUCTOS DISPONIBLES</h3>
+    <br/>
 
-		<br/>
-		
-		<?php if( isset($row) ) : ?>
+    <?php if( isset($row) ) : ?>
 
-			<table class="table">
-				<thead class="thead-dark">
-					<tr>
-					<th scope="col">#</th>
-					<th scope="col">Nombre</th>
-					<th scope="col">Marca</th>
-					<th scope="col">Modelo</th>
-					<th scope="col">Precio</th>
-					<th scope="col">Unidades</th>
-					<th scope="col">Detalles</th>
-					<th scope="col">Imagen</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th scope="row"><?= $row['id'] ?></th>
-						<td><?= $row['nombre'] ?></td>
-						<td><?= $row['marca'] ?></td>
-						<td><?= $row['modelo'] ?></td>
-						<td><?= $row['precio'] ?></td>
-						<td><?= $row['unidades'] ?></td>
-						<td><?= utf8_encode($row['detalles']) ?></td>
-						<td><img src=<?= $row['imagen'] ?> ></td>
-					</tr>
-				</tbody>
-			</table>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Marca</th>
+                <th scope="col">Modelo</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Unidades</th>
+                <th scope="col">Detalles</th>
+                <th scope="col">Imagen</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row"><?= $row['id'] ?></th>
+                    <td><?= $row['nombre'] ?></td>
+                    <td><?= $row['marca'] ?></td>
+                    <td><?= $row['modelo'] ?></td>
+                    <td><?= $row['precio'] ?></td>
+                    <td><?= $row['unidades'] ?></td>
+                    <td><?= utf8_encode($row['detalles']) ?></td>
+                    <td><img src=<?= $row['imagen'] ?> ></td>
+                </tr>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <script>
+            alert('No hay productos con unidades menores o iguales a <?= $tope ?>');
+        </script>
+    <?php endif; ?>
 
-		<?php elseif(!empty($id)) : ?>
-
-			 <script>
-                alert('El ID del producto no existe');
-             </script>
-
-		<?php endif; ?>
-	</body>
+</body>
 </html>
