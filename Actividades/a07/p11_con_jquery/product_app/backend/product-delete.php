5a5901d87d30999e77b13340315f896a2d58527c
ValePes <valeriapes2205@gmail.com>
2025-03-28 23:46:04 -0600

    Metodo de get a post

--- Actividades/a07/p11_con_jquery/product_app/backend/product-delete.php
+++ Actividades/a07/p11_con_jquery/product_app/backend/product-delete.php
@@ -2,7 +2,7 @@
     use TECWEB\MYAPI\Products as Products; 
     require_once __DIR__.'/myapi/Products.php'; 
     
-    $id = isset($_GET['id']) ? $_GET['id'] : '';
+    $id = isset($_POST['id']) ? $_POST['id'] : '';
     $prodObj = new Products('marketzone'); 
     $prodObj->delete($id); 
     echo json_encode ($prodObj->getData()); 
